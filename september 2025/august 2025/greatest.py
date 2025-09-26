f53e735a2660ce7acb374391060c986bc056c519
jasnajamal <jasnasana2163@gmail.com>
2025-09-26 10:59:49 +0530

    Add files via upload

---
+++ september 2025/august 2025/greatest.py
@@ -0,0 +1,9 @@
+a=int(input("first no:"))
+b=int(input("second no:"))
+c=int(input("third no:"))
+if((a>b)&(a>c)):
+    print("greatest is",a)
+elif((b>c)&(b>a)):
+    printf("greatest is",b)
+else:
+    print("greatest is",c)
---
+++ september 2025/august 2025/perimeter.py
@@ -0,0 +1,4 @@
+h=int(input("enter height of the rectangle: "))
+b=int(input("enter breadth of the rectangle: "))
+peri=2* (h+b)
+print("perimmeter of rectangle is ",peri)
