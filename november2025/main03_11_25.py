670d41e35e3be269a76665e515cccbd43586a5d1
jasnajamal <jasnasana2163@gmail.com>
2025-11-03 15:32:13 +0530

    Add files via upload

---
+++ november2025/main03_11_25.py
@@ -0,0 +1,16 @@
+from graphics import rectangle, circle
+from graphics.threeDgraphics import cuboid, sphere
+
+print("Rectangle area:", rectangle.area(10, 5))
+print("Rectangle perimeter:", rectangle.perimeter(10, 5))
+    
+print("Circle area:", circle.area(7))
+print("Circle perimeter:", circle.perimeter(7))
+    
+print("Cuboid surface area:", cuboid.area(2, 3, 4))
+print("Cuboid perimeter:", cuboid.perimeter(2, 3, 4))
+    
+print("Sphere surface area:", sphere.area(5))
+print("Sphere perimeter:", sphere.perimeter(5))
+
+
